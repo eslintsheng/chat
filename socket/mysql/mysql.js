e06dd3f4d035e89237603e4720a21b1b1905130e
vita <esilntsheng@gmail.com>
2022-09-12 11:11:46 +0800

    fix: connect

--- socket/mysql/mysql.js
+++ socket/mysql/mysql.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'sheng123Q',
+  password: 'sheng123Q@',
   database: 'test1',
 })
 
